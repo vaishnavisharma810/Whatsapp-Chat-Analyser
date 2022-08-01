2af3812b3b2b89632d46bc1ca8c6596fe696c844
Vaishnavi Sharma <vaishnavisharma137@gmail.com>
2022-08-01 21:20:07 +0530

    Adding all updated files

---
+++ preprocessor.py
@@ -0,0 +1,39 @@
+import re
+import pandas as pd
+
+
+def preprocess(data):  # taking a string data and returning the data frame in required format
+    pattern = '\d{1,2}/\d{1,2}/\d{2,4},\s\d{1,2}:\d{2}\s-\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    df['message_date'] = pd.to_datetime(df['message_date'], format='%m/%d/%y, %H:%M - ')
+
+    df.rename(columns={'message_date': 'date'}, inplace=True)
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(entry[2])
+        else:
+            users.append('group notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['messages'] = messages
+
+    df.drop(columns = ['user_message'], inplace=True)
+
+    df['year'] = df['date'].dt.year
+    df['month'] = df['date'].dt.month_name()
+    df['month_num'] = df['date'].dt.month
+    df['day'] = df['date'].dt.day
+    df['only_date'] = df['date'].dt.date
+    df['hour'] = df['date'].dt.hour
+    df['minute'] = df['date'].dt.minute
+    df['day_name'] = df['date'].dt.day_name()
+
+    return df
